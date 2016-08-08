c1c35ff83bb68d77b663cb87b47c507109154067
Shane Henning <shanerhenning@gmail.com>
2016-08-08 12:04:03 -0700

    error handling

--- route/route.js
+++ route/route.js
@@ -8,22 +8,34 @@ lirc.init();
 let router = module.exports = exports = new Router();
 
 router.get('/all', (req, res) => {
+  // if(lirc.remotes === { 'irsend: command not found': [ 'found' ] }){
+  //   console.log('No remotes exist on server.');
+  //   return res.sendError(AppError.error400('No remotes exist on server.'));
+  // }
   console.log(lirc.remotes);
   return res.status(200).json(lirc.remotes);
 });
 
 router.get('/:remote', (req, res) => {
+  if (!req.params.remote) {
+    return res.sendError(AppError.error400('Remote does not yet exist on server.'));
+  }
   console.log(lirc.remotes[req.params.remote]);
   return res.status(200).json(lirc.remotes[req.params.remote]);
 });
 
-router.post('/remote/:name/:button', (req, res)=>{
+router.post('/remote/:name/:button', (req, res) => {
+  if (!req.params.name) {
+    return res.sendError(AppError.error400('Invalid remote name.'));
+  } else if (!req.params.button) {
+    return res.sendError(AppError.error400('Invalid button for specified remote.'));
+  }
   lirc.irsend.send_once(req.params.name, req.params.button, () => {
     console.log('sent ' + req.params.button + ' to ' + req.params.name + '.');
     return res.status(200).send('sent ' + req.params.button + ' to ' + req.params.name + '.');
   });
 });
 
-router.all('/', (req,res)=>{
-  return res.status(404).send('Please specify a remote to use by setting your endpoint to "/api/remote-name".');
+router.all('/', (req, res, next) => {
+  next(AppError.error404('Please specify a remote to use by setting your endpoint to "/api/remote-name".'));
 });
