c0341a25d17e1a35af43bde10df8efa5a85b7070
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-09 14:52:17 -0700

    implementing socket.io in routes

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const socket = require('socket.io-client')('http://localhost:3000');
+const exec = require('child_process').exec;
+
+socket.on('time', (data) => {
+  console.log('time sent', data);
+  exec('echo ' + data);
+});
+
+socket.on('post', (data) => {
+  console.log(data);
+  exec('echo ' + data[0] + data[1], (err, stdout, stderr) => {
+    if (err) return console.log('err: ', err);
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+  });
+});
