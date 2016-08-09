101e392af8ad21914b6e25dd7d9de91f2445989f
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-08 17:25:17 -0700

    committing shit

--- model/user.js
+++ model/user.js
@@ -4,19 +4,16 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 let userSchema = mongoose.Schema({
-  username: String,
-  basic: {
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
-  }
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
 });
 
 userSchema.methods.generateHash = function(password) {
   return new Promise((resolve, reject) => {
     bcrypt.hash(password, 8, (err, data) => {
       if (err) return reject(err);
-      this.basic.password = data;
-      resolve ({token: jwt.sign({idd: this.basic.email},process.env.APP_SECRET)});
+      this.password = data;
+      resolve ({token: jwt.sign({idd: this.username},process.env.APP_SECRET)});
     });
   });
 };
@@ -25,7 +22,7 @@ userSchema.methods.comparePassword = function(password){
     bcrypt.compare(password, this.basic.password, (err, data) =>{
       if (err) return reject(err);
       if (data === false) return reject(new Error('Password did not match'));
-      resolve({token: jwt.sign({idd: this.basic.email}, process.env.APP_SECRET)});
+      resolve({token: jwt.sign({idd: this.username}, process.env.APP_SECRET)});
     });
   });
 };
--- route/auth_router.js
+++ route/auth_router.js
@@ -2,47 +2,36 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
-const Errorhandler = require('../lib/error_handler');
+const AppError = require('../lib/app_error');
 const User = require('../model/user');
 const BasicHTTP = require('../lib/basic_http');
-const authzn = require('../lib/authorization');
-const jwt_auth = require('../lib/jwt_auth');
+// const authzn = require('../lib/authorization');
+// const jwt_auth = require('../lib/jwt_auth');
 
 let authRouter = module.exports = exports = Router();
 
 authRouter.post('/signup', jsonParser, (req, res, next) => {
-    let newUser = new User();
-    newUser.basic.email = req.body.email;
-    newUser.usename = req.body.username || req.body.email;
-    newUser.generateHash(req.body.password)
-        .then((tokenData) => {
-            newUser.save().then(res.json.bind(tokenData), ErrorHandler(400, next));
-        }, ErrorHandler(500, next, 'Server Error'));
+  let newUser = new User();
+  newUser.username = req.body.username;
+  newUser.generateHash(req.body.password)
+    .then((tokenData) => {
+      newUser.save().then(res.json.bind(tokenData), AppError.error400('Bad request'));
+    }, next(AppError.error400('Bad request')));
 });
 
 authRouter.get('/signin', BasicHTTP, (req, res, next) => {
-    let authError = ErrorHandler(401, next, 'Authenticat sayz no!!');
-    User.findOne({
-            'basic.email': req.auth.username
-        })
-        .then((user) => {
-            if (!user) return authError;
-            user.comparePassword(req.auth.password)
-                .then(res.json.bind(res), authError);
-        }, authError);
+  User.findOne({'username': req.auth.username})
+    .then((user) => {
+      if (!user) return AppError.error400('Bad request');
+      user.comparePassword(req.auth.password)
+        .then(res.json.bind(res), AppError.error400('Bad request'));
+    }, next(AppError.error400('Bad request')));
 });
-
-authRouter.put('/addrole?:userid', jsonParser, jwt_auth, authzn(), (req, res, next) => {
-        user.update({
-                _id: req.params.userid
-            }, {
-                $set: {
-                    role: req.body.role
-                }
-            })
-            .then(res.json.bind(res), ErrorHandler(500, next, 'server'
-            }));
-
-    authrouter.get('/users', jsonParser, jwt_auth, authzn(), (req, res, next) => {
-        user.find().then(res.json.bind(res), ErrorHandler(500, next, 'server error'));
-    }));
+//
+// authRouter.put('/addrole?:userid', jsonParser, jwt_auth, authzn(), (req, res, next) => {
+//   user.update({'_id': req.params.userid}, {$set: {role: req.body.role}})
+//   .then(res.json.bind(res), ErrorHandler(500, next, 'server'}));
+//
+//     authrouter.get('/users', jsonParser, jwt_auth, authzn(), (req, res, next) => {
+//         user.find().then(res.json.bind(res), ErrorHandler(500, next, 'server error'));
+//     }));
