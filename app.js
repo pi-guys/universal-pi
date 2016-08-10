51e5c74c4ab1ea91877b7bc93916d54e86caa274
Shane Henning <shanerhenning@gmail.com>
2016-08-10 14:55:30 -0700

    app update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
-const socket = require('socket.io-client')('http://localhost:3000');
+// const socket = require('socket.io-client')('http://localhost:3000');
 
-// const socket = require('socket.io-client')('https://universal-pi.herokuapp.com');
+const socket = require('socket.io-client')('https://universal-pi.herokuapp.com');
 const exec = require('child_process').exec;
 // const Remote = require('./model/remote.js');
 const lirc = require('lirc_node');
@@ -11,12 +11,12 @@ console.log('running app.js');
 socket.on('post', (data) => {
   console.log('on post');
   console.log('irsend SEND_ONCE ' + data[0] + ' ' + data[1]);
-  // exec('irsend SEND_ONCE ' + data[0] + ' ' + data[1], (err, stdout, stderr) => {
-  //   console.log('irsend-ing: ', data);
-  //   if (err) return console.log('err: ', err);
-  //   console.log('stdout: ', stdout);
-  //   console.log('stderr: ', stderr);
-  // });
+  exec('irsend SEND_ONCE ' + data[0] + ' ' + data[1], (err, stdout, stderr) => {
+    console.log('irsend-ing: ', data);
+    if (err) return console.log('err: ', err);
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+  });
 });
 
 // let update = function() {
