a4bb325601a8e7b0577b8a44bdd17ab553c451af
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-09 17:54:41 -0700

    moved command route into _server file

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const exec = require('child_process').exec;
 
 socket.on('time', (data) => {
   console.log('time sent', data);
-  exec('echo ' + data);
+  return exec('echo ' + data);
 });
 
 socket.on('post', (data) => {
   console.log(data);
-  exec('echo ' + data[0] + data[1], (err, stdout, stderr) => {
+  exec('irsend SEND_ONCE ' + data[0] + ' ' + data[1], (err, stdout, stderr) => {
     if (err) return console.log('err: ', err);
     console.log('stdout: ', stdout);
     console.log('stderr: ', stderr);
