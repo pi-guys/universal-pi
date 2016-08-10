254bdd712804044c46f083034e58dd4140212019
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-10 12:35:50 -0700

    wtf merge conflict

--- test/test_server.js
+++ test/test_server.js
@@ -1,8 +1,9 @@
+'use strict';
 const mongoose = require('mongoose');
 const jwt_auth = require('../lib/jwt_auth');
 
 mongoose.connect('mongodb://localhost/auth_test');
-app = require('express')();
+let app = require('express')();
 const authRouter = require('../route/user_router');
 app.use('/api', authRouter);
 app.get('/api/jwt_auth', jwt_auth, function(req, res){
