905d6f3e077e25e4cbe69200a5146cb1b8e87824
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-10 20:50:44 -0700

    removed test_harness file. migrating to test and test_server

--- test/test.js
+++ test/test.js
@@ -2,20 +2,32 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const request = chai.request;
+// const request = chai.request;
 const expect = chai.expect;
 const mongoose = require('mongoose');
 
 const baseUrl = 'localhost:5000/api';
-const User = require('../model/user');
+// const User = require('../model/user');
 
-describe('authentication', function() {
-  it('should create a user', function(done) {
+process.env.APP_SECRET = 'test';
+process.env.MONGODB_URI = 'mongodb://localhost/dev_test';
+
+describe('test users in database', function() {
+  before(function(done) {
+    require('../server');
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dev_test');
+    done();
+  });
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(() => console.log('test_db dropped'));
+    done();
+  });
+  it('/POST should create a user', function(done) {
     chai.request(baseUrl)
       .post('/signup')
       .send({
-        username: 'test@example.com',
-        password: 'foobar123'
+        username: 'jashgu',
+        password: 'eatsleep'
       })
       .end(function(err, res) {
         expect(err).to.eql(null);
